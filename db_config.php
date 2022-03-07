19341de38b09f22a6f9b7c75da89601aaa70aaa1
Dinh Gia Bao <67374182+dinhgiabao2011@users.noreply.github.com>
2022-03-07 10:16:19 +0700

    Add files via upload

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+$host_name = "ec2-54-83-21-198.compute-1.amazonaws.com";
+$db_name = "deh33q1o6optcu";
+$db_user = "qoxwirmfwuhhac";
+$db_passwd = "97f888029afa4b445a96f2e79e26c609b8d938f7c8741eef045596faf905ebda";
+$db_conn_string = "host=$host_name dbname=$db_name user=$db_user password=$db_passwd";
+?>
